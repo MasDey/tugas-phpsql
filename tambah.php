bfaa398d2c112763a0cd755d287fd1bf78f25408
Dandi Oktavian <82819227+MasDey@users.noreply.github.com>
2021-04-22 19:52:31 +0700

    menambahkan permak.css
    menghapus style di tambah.php dan ganti ke link permak.php

--- tambah.php
+++ tambah.php
@@ -26,16 +26,7 @@ if (isset($_POST["submit"])) {
 <head>
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <style>
-        body {
-            background: #feffde;
-        }
-
-        a {
-            text-decoration: none;
-            color: white;
-        }
-    </style>
+    <link rel="stylesheet" href="permak.css">
 </head>
 
 <body>

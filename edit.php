9bea572c325c80fef0b98babc2e52ffa608ebcca
Dandi Oktavian <82819227+MasDey@users.noreply.github.com>
2021-04-23 10:32:51 +0700

    add permak.css

--- edit.php
+++ edit.php
@@ -11,16 +11,7 @@ $result = mysqli_query($conn, "SELECT*FROM mahasiswa WHERE id=$id");
 <head>
     <title>Update</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <style>
-        body {
-            background: #feffde;
-        }
-
-        a {
-            text-decoration: none;
-            color: white;
-        }
-    </style>
+    <link rel="stylesheet" href="permak.css">
 </head>
 
 <body>
